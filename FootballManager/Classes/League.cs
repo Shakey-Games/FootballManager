f0cc699af697a140464f76a8ac15da5a5614cdf0
Shaker Darwazeh <darwazehshaker@gmail.com>
2025-02-26 21:27:43 +0000

    Update League.cs

--- FootballManager/Classes/League.cs
+++ FootballManager/Classes/League.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,11 +12,13 @@ namespace FootballManager.Classes
         public List<Club> Clubs { get; set; }
         public List<Player> Players { get; set; }
         public List<Coach> Coaches { get; set; }
+        public List<Match> matches { get; set; }
         public League()
         {
             Name = "Premier League";
             Clubs = new List<Club>();
             Players = new List<Player>();
+            matches = new List<Match>();
             Coaches = new List<Coach>();
             InitialiseClubs();
             InitialisePlayers();
@@ -93,11 +95,24 @@ namespace FootballManager.Classes
             }
         }
 
-        public void SimulateSeason()
+        public void ShowLeagueTable()
         {
-            Console.WriteLine($"Starting {Name} season...");
+            var sortedClubs = Clubs.OrderByDescending(club => club.Points).ToList();
 
-            List<Match> matches = new List<Match>();
+            Console.WriteLine("League Table:\n");
+            Console.WriteLine("{0,-10} | {1,-25} | {2,6}\n", "Position", "Club", "Points");
+
+            for (int i = 0; i < sortedClubs.Count; i++)
+            {
+                Console.WriteLine("{0,-10} | {1,-25} | {2,6}", i + 1, sortedClubs[i].Name, sortedClubs[i].Points);
+            }
+        }
+
+        public void CreateSchedule()
+        {
+            // Shuffle the clubs
+            Random rng = new Random();
+            Clubs = Clubs.OrderBy(x => rng.Next()).ToList();
 
             // Create a round-robin schedule
             int numClubs = Clubs.Count;
@@ -118,32 +133,36 @@ namespace FootballManager.Classes
             }
 
             // Add reverse fixtures for the second half of the season
-            int totalWeeks = 38;
             List<Match> reverseMatches = new List<Match>();
             foreach (var match in matches)
             {
                 reverseMatches.Add(new Match(match.AwayClub, match.HomeClub));
             }
             matches.AddRange(reverseMatches);
+        }
+
+        public void SimulateGameWeek(int week)
+
+        {
+            Console.Clear();
+            int numClubs = Clubs.Count;
+
 
             // Simulate each game week
-            for (int week = 1; week <= totalWeeks; week++)
+
+
+            Console.WriteLine($"\nGame Week {week}:");
+
+            for (int i = 0; i < numClubs / 2; i++)
             {
-                Console.WriteLine($"\nGame Week {week}:");
-
-                for (int i = 0; i < numClubs / 2; i++)
+                int matchIndex = (week - 1) * (numClubs / 2) + i;
+                if (matchIndex < matches.Count)
                 {
-                    int matchIndex = (week - 1) * (numClubs / 2) + i;
-                    if (matchIndex < matches.Count)
-                    {
-                        Match match = matches[matchIndex];
-                        match.PlayMatch();
-                    }
+                    Match match = matches[matchIndex];
+                    match.PlayMatch();
                 }
-
-                Console.WriteLine("\nPress any key to proceed to the next game week...");
-                Console.ReadKey();
             }
+
         }
     }
 }
