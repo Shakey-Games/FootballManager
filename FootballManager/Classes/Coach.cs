1b6e779d2e881f853e3c73a7e6541779c27612be
Shaker Darwazeh <darwazehshaker@gmail.com>
2025-09-03 10:20:15 +0100

    Update Coach.cs

--- FootballManager/Classes/Coach.cs
+++ FootballManager/Classes/Coach.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,17 +9,32 @@ namespace FootballManager.Classes
     internal class Coach : Person
     {
         public Club Club { get; set; }
+        public List<Email> Inbox { get; set; } = new List<Email>();
 
-        public Coach(string firstName, string lastName, int age, string nationality, string club)
+
+        public Coach(string firstName, string lastName, int age, string nationality)
             : base(firstName, lastName, age, nationality)
         {
-
         }
 
         public void ShowClub()
         {
-            Console.WriteLine($"{Name} is the manager of {Club.Name}.");
+            if (Club != null)
+            {
+                Console.WriteLine($"{Name} is the manager of {Club.Name}.");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} is currently unemployed.");
+            }
         }
 
+        public void ReceiveEmail(string subject, string sender, string body)
+        {
+            Inbox.Add(new Email(subject, sender, body));
+        }
+
+
     }
+
 }
