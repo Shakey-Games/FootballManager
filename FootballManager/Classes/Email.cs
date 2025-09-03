1cea807639c0d97e8b0a1ecb9022f4075bdf59f5
Shaker Darwazeh <darwazehshaker@gmail.com>
2025-09-03 10:22:44 +0100

    Add files via upload

---
+++ FootballManager/Classes/Email.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballManager.Classes
+{
+    public class Email
+    {
+        public string Subject { get; set; }
+        public string Sender { get; set; }
+        public string Body { get; set; }
+        public bool IsRead { get; set; }
+
+        public Email(string subject, string sender, string body)
+        {
+            Subject = subject;
+            Body = body;
+            Sender = sender;
+            IsRead = false;
+        }
+
+        public override string ToString()
+        {
+            return $"{(IsRead ? "[READ] " : "[NEW] ")}{Subject} - {Sender}";
+        }
+    }
+}
---
+++ FootballManager/Classes/TransferOffer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballManager.Classes
+{
+    internal class TransferOffer
+    {
+        public Club FromClub { get; set; }
+        public Club ToClub { get; set; }
+        public Player Player { get; set; }
+        public int Fee { get; set; }
+        public bool IsUserInvolved => FromClub == Program.user.Club || ToClub == Program.user.Club;
+
+        public TransferOffer(Club fromClub, Club toClub, Player player, int fee)
+        {
+            FromClub = fromClub;
+            ToClub = toClub;
+            Player = player;
+            Fee = fee;
+        }
+
+        public override string ToString()
+        {
+            return $"{FromClub.Name} -> {ToClub.Name}: {Player.Name} for £{Fee:N0}";
+        }
+
+    }
+}
